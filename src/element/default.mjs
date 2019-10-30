7c7dbdc42dc4b22aaa03bc01dcebc3af916ba42c
Rafael Hengles <rhengles@gmail.com>
2019-10-29 23:05:02 -0300

    [wip] refactor element handlers

--- src/element/default.mjs
+++ src/element/default.mjs
@@ -3,41 +3,26 @@ export function elementInit() {
 	return {};
 }
 
-export function Name(key) {
-	this.key = key || 'name';
-}
-Name.prototype.get = function(el) {
-	return el[this.key];
-};
-Name.prototype.set = function(el, name) {
-	el[this.key] = name;
-};
-Name.prototype.init = function(el) {
-	el[this.key] = null;
+export const elementName = (key) => {
+	key || (key='name');
+	return {
+		get: (el) => el[key],
+		set: (el, name) => void (el[key] = name)
+	};
 };
 
-export function Attributes(key) {
-	this.key = key || 'attrs';
-}
-Attributes.prototype.add = function(el, attr) {
-	el[this.key].push(attr);
-};
-Attributes.prototype.init = function(el) {
-	el[this.key] = [];
+export const elementAttributes = (key) => {
+	key || (key='attrs');
+	return {
+		add: (el, attr) => void el[key].push(attr)
+	};
 };
 
-export function Children(key) {
-	this.key = key || 'children';
-}
-Children.prototype.addElement = function(el, child) {
-	el[this.key].push(child);
-};
-Children.prototype.addText = function(el, text) {
-	el[this.key].push(text);
-};
-Children.prototype.getCount = function(el) {
-	return el[this.key].length;
-};
-Children.prototype.init = function(el) {
-	el[this.key] = [];
+export const elementChildren = (key) => {
+	key || (key='children');
+	return {
+		addElement: (el, child) => void el[key].push(child),
+		addText: (el, text) => void el[key].push(text),
+		getCount: (el) => el[key].length
+	};
 };
